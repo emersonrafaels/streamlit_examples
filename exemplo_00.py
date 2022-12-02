8a01610a16156d0b2399ee263e1e8255e9d741b3
Emerson Rafael <emersona7x@hotmail.com>
2022-12-02 10:30:51 -0300

    [ADD] EXEMPLO 00, CONTENDO TITLE E MARKDOWN

---
+++ exemplo_00.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Olá internet!!!!')
+
+st.markdown('''
+# Olá, esse é um título em markdown
+**negrito**, *italico*
+## subtitilo
+''')
+
+st.image('https://via.placeholder.com/500x450')
