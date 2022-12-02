369437c4b12d4d7b33cd72c3607585ea132a0489
Emerson Rafael <emersona7x@hotmail.com>
2022-12-02 11:19:11 -0300

    [CHG] ADICIONANDO COMENTÁRIOS

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
+# CRIANDO UM TÍTULO
 st.title('Olá internet!!!!')
 
+# CRIANDO UM MARKDOWN
 st.markdown('''
 # Olá, esse é um título em markdown
 **negrito**, *italico*
 ## subtitilo
 ''')
 
+# INCLUINDO UM WIDGET DE IMAGEM
 st.image('https://via.placeholder.com/500x450')
